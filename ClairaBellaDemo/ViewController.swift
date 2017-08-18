c6c6047bc44878dd08bc12dbfaaaf2addfc21ff5
Vikash Kumar <biwal.vikash@gmail.com>
2017-08-18 09:16:03 +0530

    -

--- ClairaBellaDemo/ViewController.swift
+++ ClairaBellaDemo/ViewController.swift
@@ -10,16 +10,120 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var chartersChoice = [String : Any]()
+    var part_mapJson = [String : Any]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.getCharters()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    func getCharters() {
+        APICall.shared.characterAPICall { (json, isSuccess) in
+            if isSuccess {
+                if let json = json as? [String : Any] {
+                    print(json)
+                    if let characters = json["character"] as? [String : Any] {
+                        self.chartersChoice = characters["choices"] as! [String  :Any]
+                        self.get_partMapData()
+                    }
+                }
+            }
+        }
+    }
+
+    func get_partMapData() {
+        APICall.shared.partsMap_APICall { (json, isSuccess) in
+            if isSuccess {
+                if let json = json as? [String : Any] {
+                    print(json)
+                    self.part_mapJson = json
+                    self.get_Parts()
+                }
+            }
+
+        }
+    }
+    
+    func get_Parts() {
+        APICall.shared.parts_APICall { (json, isSuccess) in
+            if isSuccess {
+                if let json = json as? [String : Any] {
+                    print(json)
+                    self.part_mapJson = json
+                }
+            }
+        }
+    }
+}
+
+class APICall {
+    static let shared = APICall()
+    
+    
+    func characterAPICall(block: @escaping (Any?, Bool)-> Void) {
+        let urlString = "http://34.252.124.216/midnight/system/api/character.json"
+        let url = URL(string: urlString)!
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let data = data {
+                if let json = try? JSONSerialization.jsonObject(with: data, options: [.mutableContainers]) {
+                    return block(json, true)
+                    
+                }
+            }
+            
+            block(nil, false)
+        }.resume()
+        
+    }
+    
+    
+    func partsMap_APICall(block: @escaping (Any?, Bool)-> Void) {
+        let urlString = "http://34.252.124.216/midnight/system/api/parts_map.json"
+        let url = URL(string: urlString)!
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let data = data {
+                if let json = try? JSONSerialization.jsonObject(with: data, options: [.mutableContainers]) {
+                    return block(json, true)
+                    
+                }
+            }
+            
+            block(nil, false)
+        }.resume()
+
+    }
+    
+    func parts_APICall(block: @escaping (Any?, Bool)-> Void) {
+        let urlString = "http://34.252.124.216/midnight/system/api/parts.json"
+        let url = URL(string: urlString)!
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let data = data {
+                if let json = try? JSONSerialization.jsonObject(with: data, options: [.mutableContainers]) {
+                    return block(json, true)
+                }
+            }
+            
+            block(nil, false)
+        }.resume()
+        
+    }
+    
+    func parts_meta_APICall(block: @escaping (Any?, Bool)-> Void) {
+        let urlString = "http://34.252.124.216/midnight/system/api/parts_meta.json"
+        let url = URL(string: urlString)!
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let data = data {
+                if let json = try? JSONSerialization.jsonObject(with: data, options: [.mutableContainers]) {
+                    return block(json, true)
+                }
+            }
+            
+            block(nil, false)
+        }.resume()
+        
     }
 
 
 }
-
