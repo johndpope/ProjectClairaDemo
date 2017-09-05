0b6d831aaaab385e0a305067a0f3195b5343c0c8
Vikash Kumar <biwal.vikash@gmail.com>
2017-09-06 00:48:08 +0530

    -

--- ClairaBellaDemo/AppDelegate.swift
+++ ClairaBellaDemo/AppDelegate.swift
@@ -19,9 +19,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         CharacterHTMLBuilder.shared.loadBuildData()
+        self.setTabbarAppearance()
         return true
     }
 
+    func setTabbarAppearance() {
+        UITabBar.appearance().tintColor = UIColor(red: 225.0/255.0, green: 57.0/255.0, blue: 169.0/255.0, alpha: 1.0)
+        
+        UITabBarItem.appearance().badgeColor = UIColor(red: 74.0/255.0, green: 144.0/255.0, blue: 226.0/255.0, alpha: 1.0)
+        
+        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName:UIColor(red: 225.0/255.0, green: 57.0/255.0, blue: 169.0/255.0, alpha: 1.0)], for: .selected)
+        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.lightGray], for: .normal)
+
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
