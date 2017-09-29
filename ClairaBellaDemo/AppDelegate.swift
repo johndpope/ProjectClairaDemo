d10f8d09cf694dd5e98d27ecbe32482e5adcb114
Vikash Kumar <vikash.ideveloper@gmail.com>
2017-09-29 16:40:27 +0530

    -

--- ClairaBellaDemo/AppDelegate.swift
+++ ClairaBellaDemo/AppDelegate.swift
@@ -47,7 +47,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     //Facebook Delegate Methods
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-        return true//FBSDKApplicationDelegate.sharedInstance().application(app, open: url, options: options)
+        return FBSDKApplicationDelegate.sharedInstance().application(app, open: url, options: options)
     }
 
     
