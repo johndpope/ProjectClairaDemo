71a847d1e3bceb3338d3e7b4ffa81f8baf7997f2
Vikash Kumar <vikash.ideveloper@gmail.com>
2017-11-07 18:26:58 +0530

    - login and signup screen new design done

--- ClairaBellaDemo/ViewControllers/LoginSignupVCs/SignUpVC.swift
+++ ClairaBellaDemo/ViewControllers/LoginSignupVCs/SignUpVC.swift
@@ -14,151 +14,106 @@ class SignUpVC: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var emailTextField: UITextField!
-    @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var lastNameTextField: UITextField!
-    
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var tblSignupForm: UITableView!
+    @IBOutlet var tblHeaderView: UIView!
     @IBOutlet var btn_pressed: UIButton!
+    @IBOutlet var errorListView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //self.createLayout()
-        
-        // Do any additional setup after loading the view.
+        var hdvFrame = tblHeaderView.frame
+        hdvFrame.size.height = self.view.frame.height-64
+        tblHeaderView.frame = hdvFrame
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(nf:)), name: Notification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(nf:)), name: Notification.Name.UIKeyboardWillHide, object: nil)
+
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
     }
     
-    func createLayout()
-    {
-        let scrollView = UIScrollView(frame :CGRect(x: 0, y: 0, width: SCREEN_WIDTH, height: SCREEN_HEIGHT))
-        //scrollView.contentSize = CGSize(width: SCREEN_WIDTH, height:SCREEN_HEIGHT+100)
-        self.view.addSubview(scrollView)
-
-        let backBtn = UIButton(type: UIButtonType.custom) as UIButton
-        backBtn.frame = CGRect(x:0, y:20, width: SCREEN_HEIGHT*0.075, height: SCREEN_HEIGHT*0.075)
-        backBtn.setImage(UIImage(named: "BackArrow"), for: .normal)
-        backBtn.titleLabel?.font = UIFont.systemFont(ofSize: 20)
-        backBtn.addTarget(self, action: #selector(self.backClick(_:)), for: UIControlEvents.touchUpInside)
-        scrollView.addSubview(backBtn)
-        
-        let title = UIButton(type: UIButtonType.custom) as UIButton
-        title.frame = CGRect(x:SCREEN_WIDTH*0.25, y:20, width: SCREEN_WIDTH*0.50, height: SCREEN_HEIGHT*0.075)
-        title.setTitle("Join ClaireaBella", for: .normal)
-        //title.titleLabel?.font = UIFont.systemFont(ofSize: 16)
-        title.titleLabel?.font = UIFont(name: FONT_LIGHT , size: 16)
-        title.backgroundColor = PINK_LIGHT
-        scrollView.addSubview(title)
-        
-        let lineHeaderView = UIView(frame: CGRect(x:0,y:title.frame.origin.y+title.frame.size.height-5,width:SCREEN_WIDTH,height:1.5))
-        lineHeaderView.backgroundColor = UIColor.white
-        scrollView.addSubview(lineHeaderView)
-        
-//        let fblogInBtn = UIButton(type: UIButtonType.custom) as UIButton
-//        fblogInBtn.frame = CGRect(x:SCREEN_WIDTH*0.05, y:SCREEN_HEIGHT*0.13, width: SCREEN_WIDTH*0.90, height: 54)
-//        fblogInBtn.addTarget(self, action: #selector(self.Btn_Facebook_Login(_:)), for: UIControlEvents.touchUpInside)
-//        fblogInBtn.setBackgroundImage(UIImage(named :"Btn_Facebook_SignUP"), for: .normal)
-//        scrollView.addSubview(fblogInBtn)
-//        
-//        let or = UIButton(type: UIButtonType.custom) as UIButton
-//        or.frame = CGRect(x:SCREEN_WIDTH*0.10, y:fblogInBtn.frame.origin.y+fblogInBtn.frame.size.height+15, width: SCREEN_WIDTH*0.80, height: 20)
-//        or.setTitle("Or Sign Up With Email", for: .normal)
-//        or.titleLabel?.font = UIFont.systemFont(ofSize: 14)
-//        or.titleLabel?.font = UIFont(name: FONT_BOLD , size: 16)
-//        or.backgroundColor = PINK_LIGHT
-//        scrollView.addSubview(or)
-        
-        emailTextField = UITextField(frame: CGRect(x:SCREEN_WIDTH*0.05, y:SCREEN_HEIGHT*0.13, width:SCREEN_WIDTH*0.90, height:50))
-        //emailTextField.placeholder = "Email Address"
-        emailTextField.attributedPlaceholder = NSAttributedString(string: "Email Address",
-                                                                     attributes: [NSForegroundColorAttributeName: UIColor.black])
-        emailTextField.textColor = UIColor.black
-        emailTextField.textAlignment = .center
-        emailTextField.autocapitalizationType = .none
-        emailTextField.autocorrectionType = .no
-        emailTextField.keyboardType = .emailAddress
-        emailTextField.returnKeyType = .default
-        emailTextField.contentVerticalAlignment = .center
-        emailTextField.delegate = self
-        //emailTextField.backgroundColor = UIColor.white
-        emailTextField.background = UIImage(named:"Button_Back")
-        scrollView.addSubview(emailTextField)
-        
-        nameTextField = UITextField(frame: CGRect(x:SCREEN_WIDTH*0.05, y:emailTextField.frame.origin.y+emailTextField.frame.size.height+10, width:SCREEN_WIDTH*0.90, height:50))
-        //nameTextField.placeholder = "Name"
-        nameTextField.attributedPlaceholder = NSAttributedString(string: "First Name",
-                                                                  attributes: [NSForegroundColorAttributeName: UIColor.black])
-        nameTextField.textColor = UIColor.black
-        nameTextField.textAlignment = .center
-        nameTextField.autocapitalizationType = .none
-        nameTextField.autocorrectionType = .no
-        nameTextField.keyboardType = .default
-        nameTextField.returnKeyType = .default
-        nameTextField.contentVerticalAlignment = .center
-        nameTextField.delegate = self
-        //nameTextField.backgroundColor = UIColor.white
-        nameTextField.background = UIImage(named:"Button_Back")
-        scrollView.addSubview(nameTextField)
-        
-        lastNameTextField = UITextField(frame: CGRect(x:SCREEN_WIDTH*0.05, y:nameTextField.frame.origin.y+nameTextField.frame.size.height+10, width:SCREEN_WIDTH*0.90, height:50))
-        //lastNameTextField.placeholder = "Name"
-        lastNameTextField.attributedPlaceholder = NSAttributedString(string: "Last Name",
-                                                               attributes: [NSForegroundColorAttributeName: UIColor.black])
-        lastNameTextField.textColor = UIColor.black
-        lastNameTextField.textAlignment = .center
-        lastNameTextField.autocapitalizationType = .none
-        lastNameTextField.autocorrectionType = .no
-        lastNameTextField.keyboardType = .default
-        lastNameTextField.returnKeyType = .default
-        lastNameTextField.contentVerticalAlignment = .center
-        lastNameTextField.delegate = self
-        //lastNameTextField.backgroundColor = UIColor.white
-        lastNameTextField.background = UIImage(named:"Button_Back")
-
-        scrollView.addSubview(lastNameTextField)
-        
-        let registerBtn = UIButton(type: UIButtonType.custom) as UIButton
-        registerBtn.frame = CGRect(x:SCREEN_WIDTH*0.05, y:lastNameTextField.frame.origin.y+lastNameTextField.frame.size.height+25, width: SCREEN_WIDTH*0.90, height: 50)
-        registerBtn.addTarget(self, action: #selector(self.signUpBtnClick(_:)), for: UIControlEvents.touchUpInside)
-        registerBtn.backgroundColor = PINK
-        registerBtn.setTitle("Join", for: .normal)
-        registerBtn.titleLabel?.font = UIFont(name: FONT_LIGHT , size: 18)
-        registerBtn.layer.cornerRadius = 5.0
-        scrollView.addSubview(registerBtn)
-        
-        
-        let imgBottom = UIImageView()
-        imgBottom.frame = CGRect(x: 0, y: registerBtn.frame.origin.y+registerBtn.frame.size.height+35, width: SCREEN_WIDTH, height: 150)
-        imgBottom.contentMode = .scaleAspectFit
-        imgBottom.image = UIImage(named: "new_charcter")
-        imgBottom.backgroundColor = UIColor.clear
-        
-        scrollView.addSubview(imgBottom)
-        
-        scrollView.contentSize = CGSize(width: SCREEN_WIDTH, height:imgBottom.frame.origin.y + imgBottom.frame.size.height + 8)
+    //Keyboard notifications
+    func keyboardWillShow(nf: Notification) {
+        tblSignupForm.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 150, right: 0)
+    }
+    
+    func keyboardWillHide(nf: Notification) {
+        tblSignupForm.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
 
+    //TextField delegate
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        return textField.resignFirstResponder()
+        switch textField.tag {
+        case 1:
+            nameTextField.becomeFirstResponder()
+        case 2:
+            lastNameTextField.becomeFirstResponder()
+        case 3:
+            passwordTextField.becomeFirstResponder()
+        default:
+            textField.resignFirstResponder()
+        }
+        return true
     }
 
-    @IBAction func backClick(_ sender: UIButton)
-    {
+    
+    func isValidate()-> Bool {
+        var isValid = true
+        let email = emailTextField.text!.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let firstname = nameTextField.text!.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let lastName = lastNameTextField.text!.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let password = passwordTextField.text!.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        
+        emailTextField.setBorder(0)
+        nameTextField.setBorder(0)
+        lastNameTextField.setBorder(0)
+        passwordTextField.setBorder(0)
+        
+        if email.isEmpty || !email.isValidEmail(){
+            isValid = false
+            emailTextField.setBorder()
+        } 
+        if firstname.isEmpty {
+            isValid = false
+            nameTextField.setBorder()
+        }
+        
+        if lastName.isEmpty {
+            isValid = false
+            lastNameTextField.setBorder()
+        }
+        
+        if password.isEmpty {
+            isValid = false
+            passwordTextField.setBorder()
+        }
+        errorListView.isHidden = isValid
+        return isValid
+    }
+    
+    //IBActions
+    @IBAction func backClick(_ sender: UIButton) {
         _ = self.navigationController?.popViewController(animated: true)
     }
     
-    @IBAction func signUpBtnClick(_ sender: UIButton)
-    {
-        let email = emailTextField.text!
-        let firstname = nameTextField.text!
-        let lastName = lastNameTextField.text!
+    @IBAction func signUpBtnClick(_ sender: UIButton) {
+        if !isValidate() {return}
         
-        if email.isEmpty {return}
-        if firstname.isEmpty {return}
-        if lastName.isEmpty {return}
-        
+        let email = emailTextField.text!.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let firstname = nameTextField.text!.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let lastName = lastNameTextField.text!.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let password = passwordTextField.text!.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+
         let progressHUD = ProgressView(text: "Please Wait")
         self.view.addSubview(progressHUD)
         progressHUD.show()
@@ -233,4 +188,20 @@ class SignUpVC: UIViewController, UITextFieldDelegate {
             }
         })
     }
+}
+
+extension UITextField {
+    func setBorder(_ width: CGFloat = 1.5) {
+        self.layer.borderColor = UIColor.red.cgColor
+        self.layer.borderWidth = width
+    }
+}
+
+extension String {
+    func isValidEmail() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: self)
+    }
+
 }
